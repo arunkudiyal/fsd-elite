e4e2e4ba69de29a0d475049f85ec3b7688ed8c0f
arunkudiyal <arunkudiyal@icloud.com>
2022-04-08 16:49:04 +0530

    JS DataTypes & Implicit Typecasting

---
+++ JavaScript/main.js
@@ -0,0 +1,39 @@
+// console.log('Hello from main.js...')
+
+// console.error('This is an ERROR')
+// console.warn('This is a WARNING')
+
+// alert('THIS IS AN ALERT!')
+
+// variableOne = '100'
+// varTwo = variableOne + 23
+// // console.log(typeof variableOne)
+// console.log(varTwo)
+
+// varThree = false
+// varFour = varThree + 200
+// console.log(varFour)
+
+// result = 100 / 0
+// console.log(result)
+
+// console.log('200' + 'JavaScript')
+
+// a = null
+// console.log(typeof a)
+
+// b = undefined
+// console.log(typeof b)
+
+// console.log(100 + '200' + false);
+// console.log('String' - 'String' + false) // NaN
+// console.log('100' + '200' + true)
+
+let a = 'Arun'
+console.log(a)
+
+var b = true
+console.log(b)
+
+const c = null
+console.log(c)
