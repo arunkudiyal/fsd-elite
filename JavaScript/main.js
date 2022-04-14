7af14f7a42594b53a69423c4f1d65ecb290bf84b
arunkudiyal <arunkudiyal@icloud.com>
2022-04-14 17:12:49 +0530

    Arrays - Array Properties & Methods

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -127,9 +127,149 @@
 // console.log(str.substr(7));
 // console.log(str.substring(0, 9))
 
-// const names = 'Pavan, Ram, Arun, Pranay, Dinesh, Ayesha'
+// const names = 'Pavan, Ram, Arun,, Pranay, Dinesh, Ayesha'
 // let response = names.split(', ')
 // console.log(response)
 
 // console.log(names[3]);
 // console.log(names.charAt(3))
+
+
+// Q.1 -> Two numbers -> a / b -> if division is possible
+// let a = 100
+// let b = 20
+// let c = a / b
+
+// if(typeof a === 'number' && typeof b === 'number') {
+//     if(b === 0) {
+//         console.log('a/b is not possible')
+//     } else {
+//         console.log('a/b is possible')
+//     }
+// } else {
+//     console.log('Not Possible')
+// }
+
+// let val = 13
+
+// if(val % 2 === 0) {
+//     console.log('EVEN')
+// } else {
+//     console.log('ODD')
+// }
+
+
+// let cel = 100
+// let farh = (cel * (9/5)) + 32
+// console.log(farh)
+
+// let coupon = 'DIV50'
+// let shop = 10500
+
+// if(coupon === 'DIV50') {
+//     // 10% discount
+//     let discountedPrice = 0.1 * shop
+//     let toPay = shop - discountedPrice
+//     console.log(toPay);
+// } else {
+//     // 5% discount
+//     let discountedPrice = 0.05 * shop
+//     let toPay = shop - discountedPrice
+//     console.log(toPay)
+// }
+
+
+
+// ARRAYS
+// 1. By using Array Constructor
+
+// let numbers = new Array(100)
+// let numbers = new Array("String", "Values")
+// let names = new Array('Name-1', 'Name-2', 'Name-3', 'Name-4', 'Name-5')
+// let numbers = new Array(100, 200, 300, 400, 500)
+// console.log(numbers)
+// console.log(names)
+// console.log(numbers[2])
+// console.log(numbers.length)
+
+// 2. By using a [ ]
+// let stringArray = [ 'str1', 'str2', 'str3', 'str4', 'str5', 'str6' ]
+// console.log(stringArray)
+
+// let numArray = [ 100, 200, 300, 400, 500 ]
+// console.log(numArray)
+
+// const arr = [ 100, 'string', true, null, undefined ]
+// console.log(arr)
+
+// const arr = [ [10, 20, 30], [40, 50, 60], [79, 80, 90], [100, 110, 120] ]
+// console.log(arr[2][2]);
+
+// const fruits = ['Mango', 'Banana', 'Apple', 'Grapes', 'Orange', 'Pear']
+
+// Add an element in the array
+// const newLength = fruits.push('Pinapple')
+// console.log(newLength, fruits);
+
+// // Remove an elemnet from the array
+// const remvedElt = fruits.pop()
+// console.log(remvedElt);
+// console.log(fruits);
+
+// // Removing a data based on index
+// fruits.splice(2, 1)
+// console.log(fruits)
+
+// const index = fruits.indexOf('Orange')
+// console.log(index);
+
+
+// CLASSES & OBJECTS
+
+// class Form {
+//     constructor(name, email, password, dob, mobile) {
+//         this.name = name
+//         this.email = email
+//         this.password = password
+//         this.dob = dob
+//         this.mobile = mobile
+//     }
+//     constructor(length) {
+//         this.length = length
+//     }
+// }
+
+// // Create an object
+// let formSubmissionOne = new Form("Arun Kudiyal", "abc@xyz.com", "myPassword", "05/07/1992", 1234567890)
+
+// let formSubmissionTwo = new Form("New Name", "xyz@abc.com", "examplePwd", "01/02/1998", 345678910)
+
+// let formSubmissionThree = new Form(100)
+
+// console.log(formSubmissionOne)
+// console.log(formSubmissionTwo);
+// console.log(formSubmissionThree);
+
+
+// .. contd Arrays
+const fruits = [ "Mango", "Orange", "Banana", "Pears" ]
+
+// Add a data in the array - push()
+let newLen = fruits.push("Watermelon")
+console.log(fruits, newLen);
+
+// Delete a data from array - pop()
+let poppedVal = fruits.pop()
+console.log(poppedVal);
+console.log(fruits);
+
+// indexOf
+let index = fruits.indexOf('Watermelon')
+console.log(index)
+
+// 
+let newFruits = [ ...fruits ]
+newFruits.pop()
+
+console.log(newFruits);
+console.log(fruits)
